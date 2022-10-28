0fc1fffebd90e1ffe59a1bdbd4a14d395cc591c4
Thales Barreto <trvbarreto@gmail.com>
2022-10-28 18:43:26 -0300

    Add Dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Dashboard } from "./Components/Dashboard";
 import { Header } from "./Components/Header";
 import { GlobalStyle } from "./styles/global";
 
@@ -5,6 +6,8 @@ export function App() {
   return (
     <>
       <Header />
+
+      <Dashboard />
       <GlobalStyle />
     </>
   );
